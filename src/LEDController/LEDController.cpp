7afbaa1e45c400c9986d07f235c1ddc5b408d922
Dakhakhny <ahmed.addakhakhny@gmail.com>
2025-09-01 14:09:51 +0200

    Generate config files 	Generate config header files during the compilation, to hold some project meta-data

---
+++ src/LEDController/LEDController.cpp
@@ -0,0 +1,7 @@
+#include "LEDController.hpp"
+
+
+void LEDController::init() {
+    std::cout << project_name << " application started\n";
+    //   std::cout << "project git commit sha: " << app_git_commit_sha << std::endl;
+}
---
+++ src/LEDController/LEDController.hpp
@@ -0,0 +1,13 @@
+#ifndef LEDCONTROLLER_HPP
+#define LEDCONTROLLER_HPP
+#include <iostream>
+#include "include/LEDControllerConfig.hpp"
+
+class LEDController {
+public:
+    LEDController() = default;
+    void init();
+private:
+    //
+};
+#endif
