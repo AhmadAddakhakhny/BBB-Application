b9617697e357baabb32f94c66017e77570e474b0
Dakhakhny <ahmed.addakhakhny@gmail.com>
2025-10-07 11:17:49 +0200

    Add TBD features 	- Consider implementing a script to fetch the latest SDK 	- Cosider implementing a wraper script to source env-setup

---
+++ opt/environment-setup.sh
@@ -0,0 +1,6 @@
+# TBD:
+#=====
+
+# To make things portable, add opt/environment-setup.sh that detects and sources the right SDK setup:
+# If you want your scripts/ directory to help bootstrap everything,
+# you can add a simple shortcut there — for example, scripts/setup-env.sh:
---
+++ opt/install-sdk.sh
@@ -0,0 +1,5 @@
+# TBD:
+#=====
+
+# This script makes it easy to install or update the SDK locally without committing binaries.
+# You can optionally publish the SDK .sh file via GitHub Releases, so your teammate can download it with curl or wget.
